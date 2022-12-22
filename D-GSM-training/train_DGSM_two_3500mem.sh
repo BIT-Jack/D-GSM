3d8b2c90f689b8d13c164507ff12fce5209a966c
BIT-Jack <78714499+BIT-Jack@users.noreply.github.com>
2022-12-22 16:44:27 +0800

    Update train_DGSM_two_3500mem.sh

--- D-GSM-training/train_DGSM_two_3500mem.sh
+++ D-GSM-training/train_DGSM_two_3500mem.sh
@@ -9,9 +9,11 @@ P0=$!
 #CUDA_VISIBLE_DEVICES=0 python3 train_DGSM.py --lr 0.001 --n_stgcnn 1 --n_txpcnn 5  --dataset 3-ZS --tag social-stgcnn-ZS --use_lrschd --num_epochs 250 --tasks 3 --mem_size 3500 --margin 0.5 --eps 0.001 --cur_task 2 && echo "three continuous scenarios training Launched." &
 #P1=$!
 
+#four continuous scenarios: MA(past)->FT(past)->ZS(past)->EP(current)
 #CUDA_VISIBLE_DEVICES=0 python3 train_DGSM.py --lr 0.001 --n_stgcnn 1 --n_txpcnn 5  --dataset 4-EP --tag social-stgcnn-EP --use_lrschd --num_epochs 250 --tasks 4 --mem_size 3500 --margin 0.5 --eps 0.001 --cur_task 3 && echo "four continuous scenarios training Launched." &
 #P2=$!
 
+#five continuous scenarios: MA(past)->FT(past)->ZS(past)->EP(past)->SR(current)
 #CUDA_VISIBLE_DEVICES=0 python3 train_DGSM.py --lr 0.001 --n_stgcnn 1 --n_txpcnn 5  --dataset 5-SR --tag social-stgcnn-SR --use_lrschd --num_epochs 250 --tasks 5 --mem_size 3500 --margin 0.5 --eps 0.001 --cur_task 4 && echo "five continuous scenarios training Launched." &
 #P3=$!
 
